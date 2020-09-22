ac61f1cc967d81a3fae5fa11339f240b00c75813
Katsushige Fukushima <sasnorth_sports@outlook.jp>
2020-09-22 16:29:44 +0900

    delete folder "game_2019-20"

--- v1_m_daily19-20.py
+++ v1_m_daily19-20.py
@@ -12,18 +12,18 @@ print(team_list)
 for x in team_list:
     print('チーム:{}'.format(x))
     # ディレクトリ：dailyを作成
-    daily_dir = '{0}\{1}\game_2019-20\daily'.format(division,x)
+    daily_dir = r'{0}\{1}\2019-20_regular\daily'.format(division,x)
     if not os.path.isdir(daily_dir):
         os.makedirs(daily_dir)
         
     # ファイル名だけを取得
-    path = '{0}\{1}\game_2019-20'.format(division,x)
+    path = r'{0}\{1}\2019-20_regular'.format(division,x)
     files = os.listdir(path)
     files_list = [f for f in files if os.path.isfile(os.path.join(path, f))]
     # print(files_file)
     data_list = []
     for i in files_list:
-        data = pd.read_csv(r'C:\Users\sasno\Desktop\MyPandas\vleague\{0}\{1}\game_2019-20\{2}'.format(division,x,i), encoding='cp932')
+        data = pd.read_csv('{0}/{1}/2019-20_regular/{2}'.format(division,x,i), encoding='cp932')
         # 最後の1行の値：'チーム合計'を含まないようにしている
         data_list.append(data[:-1])
     game_data = pd.concat(data_list, ignore_index = True)
@@ -32,5 +32,5 @@ for x in team_list:
     # print(len(player_list),player_list)
     for i in range(len(player_list)):
         daily_stats = game_data[game_data['名前'] == player_list[i]]
-        daily_stats.to_csv('/Users/sasno/Desktop/MyPandas/vleague/{0}/{1}/game_2019-20/daily/{2}_{3}_dailystats.csv'.format(division,x,number_list[i],player_list[i]), index=False, encoding='cp932')
+        daily_stats.to_csv('{0}/{1}/2019-20_regular/daily/{2}_{3}_dailystats.csv'.format(division,x,number_list[i],player_list[i]), index=False, encoding='cp932')
         print('{}_dailystats.csvを作成'.format(i))
--- v1_w_daily19-20.py
+++ v1_w_daily19-20.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import os
 
+os.chdir('/Users/sasno/Desktop/MyPandas/vleague')
+
 division = 'v1_w'
 
-team_list = os.listdir(r'C:\Users\sasno\Desktop\MyPandas\vleague\{0}'.format(division))
+team_list = os.listdir(division)
 print(team_list)
 
 for x in team_list:
     print('チーム:{}'.format(x))
     # ディレクトリ：dailyを作成
-    daily_dir = r'C:\Users\sasno\Desktop\MyPandas\vleague\{0}\{1}\game_2019-20\daily'.format(division,x)
+    daily_dir = r'{0}\{1}\game_2019-20\daily'.format(division,x)
     if not os.path.isdir(daily_dir):
         os.makedirs(daily_dir)
         
     # ファイル名だけを取得
-    path = r'C:\Users\sasno\Desktop\MyPandas\vleague\{0}\{1}\game_2019-20'.format(division,x)
+    path = r'{0}\{1}\game_2019-20'.format(division,x)
     files = os.listdir(path)
     files_list = [f for f in files if os.path.isfile(os.path.join(path, f))]
     # print(files_list)
@@ -23,7 +25,7 @@ for x in team_list:
     if len(files_list) > 0:
         data_list = []
         for i in files_list:
-            data = pd.read_csv(r'C:\Users\sasno\Desktop\MyPandas\vleague\{0}\{1}\game_2019-20\{2}'.format(division,x,i), encoding='cp932')
+            data = pd.read_csv(r'{0}\{1}\game_2019-20\{2}'.format(division,x,i), encoding='cp932')
             # 最後の1行の値：'チーム合計'を含まないようにしている
             data_list.append(data[:-1])
         game_data = pd.concat(data_list, ignore_index = True)
@@ -31,5 +33,5 @@ for x in team_list:
         # print(len(u),u)
         for i in u:
             player_list = game_data[game_data['名前'] == i]
-            player_list.to_csv('/Users/sasno/Desktop/MyPandas/vleague/{0}/{1}/game_2019-20/daily/{2}_{3}_dailystats.csv'.format(division,x,i), index=False, encoding='cp932')
+            player_list.to_csv('{0}/game_2019-20/daily/{1}_{2}_dailystats.csv'.format(division,x,i), index=False, encoding='cp932')
             print('{}_dailystats.csvを作成'.format(i))
