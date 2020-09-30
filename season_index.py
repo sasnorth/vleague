cc312dfd8d818e948c2b96431e46d74d9b290fd4
Katsushige Fukushima <sasnorth_sports@outlook.jp>
2020-09-30 16:11:47 +0900

    delete

--- season_index.py
+++
@@ -1,7 +0,0 @@
-def get_seasons(year):
-    if year == '19-20_reg':
-        return {
-            '301':'2020-21_regular',
-            '283':'2019-20_regular',
-            '258':'2018-19_regular',
-        }
